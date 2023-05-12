2cc71673f19f75e7bc2390bc14e2e2787b459d13
Abdul Basit <dev.basit2020@gmail.com>
2023-05-12 18:16:16 +0500

    angular event filtering

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -24,6 +24,17 @@ export class CoursesComponent {
     console.log('event bubbling occuring ', event);
   }
 
+  // We need the event object here
+  traditionalEventFiltering(event: any) {
+    if (event.keyCode === 13)
+      console.log('event filtering from traditional way');
+  }
+
+  // we do not need the event object here
+  angularEventFiltering() {
+    console.log('angular event filtering feature');
+  }
+
   constructor(service: CoursesService) {
     this.courses = service.getCourses();
   }
