30c0cf5854e7b67307e2483a4f50cb95f833dc50
Abdul Basit <dev.basit2020@gmail.com>
2023-05-12 15:59:07 +0500

    two way binding - angular syntax

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +9,7 @@ import { CoursesService } from './courses.service';
 
 @NgModule({
   declarations: [AppComponent, CoursesComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [CoursesService],
   bootstrap: [AppComponent],
 })
--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -11,39 +11,57 @@ export class CoursesComponent {
   isActive = true;
   courses;
 
-  onSave() {
-    console.log('button clicked');
-  }
-
-  checkingEvent(event: any) {
-    event.stopPropagation(); // this will stop event bubbling
-    console.log('event is ', event);
-  }
-
-  affectedByEventBubbling(event: any) {
-    console.log('event bubbling occuring ', event);
-  }
-
-  // We need the event object here
-  traditionalEventFiltering(event: any) {
-    if (event.keyCode === 13)
-      console.log('event filtering from traditional way');
-  }
-
-  // we do not need the event object here
-  angularEventFiltering() {
-    console.log('angular event filtering feature');
-  }
-
-  traditionallyHandleInput(event: any) {
-    console.log('Traditional way of getting input value ', event.target.value);
-  }
-
-  angularHandleInput(name: String) {
-    console.log('Angular way of getting input value ', name);
-  }
-
   constructor(service: CoursesService) {
     this.courses = service.getCourses();
+  }
+
+  // onSave() {
+  //   console.log('button clicked');
+  // }
+
+  // checkingEvent(event: any) {
+  //   event.stopPropagation(); // this will stop event bubbling
+  //   console.log('event is ', event);
+  // }
+
+  // affectedByEventBubbling(event: any) {
+  //   console.log('event bubbling occuring ', event);
+  // }
+
+  // // We need the event object here
+  // traditionalEventFiltering(event: any) {
+  //   if (event.keyCode === 13)
+  //     console.log('event filtering from traditional way');
+  // }
+
+  // // we do not need the event object here
+  // angularEventFiltering() {
+  //   console.log('angular event filtering feature');
+  // }
+
+  // traditionallyHandleInput(event: any) {
+  //   console.log('Traditional way of getting input value ', event.target.value);
+  // }
+
+  // angularHandleInput(name: String) {
+  //   console.log('Angular way of getting input value ', name);
+  // }
+
+  oneWayBinding() {
+    console.log(
+      'author is ',
+      this.author,
+      '\n',
+      'You see name is not changing, this is one way binding'
+    );
+  }
+
+  twoWayBinding() {
+    console.log(
+      'author is ',
+      this.author,
+      '\n',
+      'You see name is changing, this is two way binding'
+    );
   }
 }
