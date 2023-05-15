ed2c5e82f994d473d86c3f2b528cd29c7b412af6
Abdul Basit <dev.basit2020@gmail.com>
2023-05-15 17:34:40 +0500

    related files moved to their own folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
-import { CoursesService } from './courses.service';
-import { SummaryPipe } from './summary.pipe';
+import { CoursesService } from './services/courses.service';
+import { SummaryPipe } from './custom-pipes/summary.pipe';
 import { FavoriteComponent } from './favorite/favorite.component';
 import { BootstrapPannelComponent } from './bootstrap-pannel/bootstrap-pannel.component';
 import { LikeComponent } from './exercises/like-component/like-component.component';
 import { ExcersicesComponent } from './exercises/excersices/excersices.component';
 import { DirectivesComponent } from './directives/directives.component';
+import { InputFormDirective } from './custom-directives/input-form.directive';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { DirectivesComponent } from './directives/directives.component';
     BootstrapPannelComponent,
     LikeComponent,
     DirectivesComponent,
+    InputFormDirective,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [CoursesService],
