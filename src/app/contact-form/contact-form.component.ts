a3105069005a1c8c6f09f4f58f9f76f1c42c7b00
Abdul Basit <dev.basit2020@gmail.com>
2023-05-16 18:32:10 +0500

    checkboxes, dropdown, and radio buttons

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -6,6 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-form.component.scss'],
 })
 export class ContactFormComponent {
+  contactMethods = [
+    { id: 1, name: 'Email' },
+    { id: 2, name: 'Phone' },
+  ];
+
   log(x: any) {
     console.log(x);
   }
